96e12dfdadd3f9e4e2590a736b231c9933ac8563
Sudarshan Acharya <im.sudarshan09@gmail.com>
2021-04-27 12:20:52 -0500

    clean up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-require('dotenv').config({path: __dirname + '../.env'});
+require('dotenv').config({path: __dirname + './../.env'});
 const axios = require("axios");
 const app = express();
 
