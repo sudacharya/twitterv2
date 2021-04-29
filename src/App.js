8ba2d61548eb0a971de9904eaec6ef9476f7fe47
Sudarshan Acharya <im.sudarshan09@gmail.com>
2021-04-29 01:16:16 -0500

    script update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/nav";
 import GlobalStyles, { Footer } from "./GlobalStyles";
-//import TweetFeed from "./components/TweetFeed"
 import Feed from "./components/Feed"
 import { MenuProvider } from "./state";
 
@@ -16,7 +15,6 @@ class App extends React.Component {
         <MenuProvider>
           <Navbar />
         </MenuProvider>
-        
         <Route exact path="/" component={() => <Feed id='783214' />} />
         <Route exact path="/elon" component={() => <Feed id="44196397" />} />
         <Route exact path="/POTUS" component={() => <Feed id="1349149096909668363" />} />
