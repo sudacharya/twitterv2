b365539bff58c3bdb17fc0c1b83b83d826e6baac
Sudarshan Acharya <sud@Sudarshans-MBP.lan>
2024-09-04 12:51:34 -0500

    format

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <GlobalStyles/>   
-      <BrowserRouter>
-        <MenuProvider>
-          <Navbar />
-        </MenuProvider>
-        <Route exact path="/" component={() => <Feed id='783214' />} />
-        <Route exact path="/elon" component={() => <Feed id="44196397" />} />
-        <Route exact path="/POTUS" component={() => <Feed id="1349149096909668363" />} />
-        <Footer />
-      </BrowserRouter>     
+        <GlobalStyles />
+        <BrowserRouter>
+          <MenuProvider>
+            <Navbar />
+          </MenuProvider>
+          <Route exact path="/" component={() => <Feed id='783214' />} />
+          <Route exact path="/elon" component={() => <Feed id="44196397" />} />
+          <Route exact path="/POTUS" component={() => <Feed id="1349149096909668363" />} />
+          <Footer />
+        </BrowserRouter>
       </>
     );
   }
